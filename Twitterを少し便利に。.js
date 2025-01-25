c843f32c464afe076d58deee6a4bded8b78fc203
ユニティー <91655582+Happy-come-come@users.noreply.github.com>
2025-01-25 23:54:27 +0900

    「メディア欄に全ての画像を表示」の修正
    ・メディア欄のクラス名が変更されたので修正
    ・ページ数の表示がズレていたので修正

--- Twitterを少し便利に。.js
+++ Twitterを少し便利に。.js
@@ -3,7 +3,7 @@
 // @name:ja			Twitterを少し便利に。
 // @name:en			Make Twitter little useful.
 // @namespace		https://greasyfork.org/ja/users/1023652
-// @version			2.1.0.2
+// @version			2.1.0.3
 // @description			私の作ったスクリプトをまとめたもの。と追加要素。
 // @description:ja			私の作ったスクリプトをまとめたもの。と追加要素。
 // @description:en			A compilation of scripts I've made.
@@ -1310,7 +1310,7 @@
 		}
 		async function expand(){
 			const mediaPlace = document.querySelector('[data-testid="primaryColumn"] section');
-			const madiaRow = mediaPlace.querySelectorAll(`.r-18u37iz.r-9aw3ui.r-14gqq1x.r-1xpp3t0:not(.Show_all_Medias_checked)`);
+			const madiaRow = mediaPlace.querySelectorAll(`.r-18u37iz.r-9aw3ui.r-1537yvj.r-14gqq1x:not(.Show_all_Medias_checked)`);
 			const blurSvgPath = 'path[d="M3.693 21.707l-1.414-1.414 2.429-2.429c-2.479-2.421-3.606-5.376-3.658-5.513l-.131-.352.131-.352c.133-.353 3.331-8.648 10.937-8.648 2.062 0 3.989.621 5.737 1.85l2.556-2.557 1.414 1.414L3.693 21.707zm-.622-9.706c.356.797 1.354 2.794 3.051 4.449l2.417-2.418c-.361-.609-.553-1.306-.553-2.032 0-2.206 1.794-4 4-4 .727 0 1.424.192 2.033.554l2.263-2.264C14.953 5.434 13.512 5 11.986 5c-5.416 0-8.258 5.535-8.915 7.001zM11.986 10c-1.103 0-2 .897-2 2 0 .178.023.352.067.519l2.451-2.451c-.167-.044-.341-.067-.519-.067zm10.951 1.647l.131.352-.131.352c-.133.353-3.331 8.648-10.937 8.648-.709 0-1.367-.092-2-.223v-2.047c.624.169 1.288.27 2 .27 5.415 0 8.257-5.533 8.915-7-.252-.562-.829-1.724-1.746-2.941l1.438-1.438c1.53 1.971 2.268 3.862 2.33 4.027z"]';
 			if(madiaRow.length === 0)return;
 			const screenName = currentUrl.split('/')[3];
@@ -1333,8 +1333,8 @@
 				numberSpan.textContent = '1';
 				numberSpan.style.position = 'absolute';
 				numberSpan.style.color = 'black';
-				numberSpan.style.left = 'calc(88.5%)';
-				numberSpan.style.top = 'calc(87.5%)';
+				numberSpan.style.right = '15px';
+				numberSpan.style.bottom = '4px';
 				numberSpan.className = 'indexNum';
 				svgElement.parentNode.appendChild(numberSpan);
 			});
