4e8a36f7e52773830f962cc236668f6bfb0cca63
ユニティー <91655582+Happy-come-come@users.noreply.github.com>
2026-05-05 10:05:15 +0900

    twitterIDとpixivIDの対応表を更新

--- Twitterを少し便利に。.js
+++ Twitterを少し便利に。.js
@@ -3,7 +3,7 @@
 // @name:ja			Twitterを少し便利に。
 // @name:en			Make Twitter a Little more Useful.
 // @namespace		https://greasyfork.org/ja/users/1023652
-// @version			2.6.0.6
+// @version			2.6.0.7
 // @description			で？みたいな機能の集まりだけど、きっとTwitterを少し便利にしてくれるはず。
 // @description:ja			で？みたいな機能の集まりだけど、きっとTwitterを少し便利にしてくれるはず。
 // @description:en			It's a collection of features like "So what?", but it will surely make Twitter a little more useful.
@@ -3367,7 +3367,7 @@ button[data-testid="UserCell"] div:has(> [href="https://help.x.com/rules-and-pol
 	}
 
 	async function getPixivLinkCollection(){
-		const pixivLinkCollectionDatabeseVersion = 20260105;
+		const pixivLinkCollectionDatabeseVersion = 20260505;
 		const fileUrl = 'https://raw.githubusercontent.com/Happy-come-come/UserScripts/refs/heads/main/Twitter%E3%82%92%E5%B0%91%E3%81%97%E4%BE%BF%E5%88%A9%E3%81%AB%E3%80%82/data/screenName2PixivID.json';
 		const thisStoredData = scriptDataStore?.makeTwitterLittleUseful?.pixivLinkCollection;
 		if(!thisStoredData?.dataBaseVersion || (thisStoredData?.dataBaseVersion < pixivLinkCollectionDatabeseVersion)){
